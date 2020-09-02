d92c622dd25acdc39fea38931b5ac37d66bb413e
Manjunath Reddy <manju16832003@gmail.com>
2020-09-02 19:15:24 +0800

    Tuples

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # erlang-playground
 Erlang Playground
+
+## How to run each program
+
+```
+$ erl
+$ c(module)
+$ module:function_name
+
+Ex:
+$erl
+$> c(tuples)
+$> tuples:point(2,5)
+```
--- tuples.erl
+++ tuples.erl
@@ -1,5 +1,11 @@
 -module(tuples).
--export([point/2]).
+-export([point/2, convert_length/1]).
 
 point(X, Y) ->
     {X, Y}.
+
+convert_length({centimeter, X}) ->
+    {inch, X / 2.54};
+
+convert_length({inch, Y}) -> 
+    {centimeter, Y * 2.54}.
