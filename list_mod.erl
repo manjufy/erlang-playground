8d2766e909269ce6339a8a380579f3f0d22f0527
Manjunath Reddy <manju16832003@gmail.com>
2020-09-04 10:24:45 +0800

    List length fun

--- list_mod.erl
+++ list_mod.erl
@@ -1,9 +1,15 @@
 %% Lists
 -module(list_mod).
--export([list/0, ops/0]).
+-export([list/0, ops/0, list_length/1]).
 
 list() ->
     [{lang, "erlang"}].
 
 ops() ->
     [FIRST | REST] = [1,2,3,4,5,6,7].
+
+list_length([]) ->
+    0;
+
+list_length([First | Last]) ->
+    1 + list_length(Last).
