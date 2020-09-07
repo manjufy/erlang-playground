a61e4052bcc73d09065e193878f0124ab9f3ac82
Manjunath Reddy <manju16832003@gmail.com>
2020-09-07 21:07:12 +0800

    Add tests

--- geometry.erl
+++ geometry.erl
@@ -1,5 +1,10 @@
 -module(geometry).
--export([area/1]).
+-export([test/0, area/1]).
+
+test() ->
+    12 = area({rectangle, 3, 4}),
+    144 = area({square, 12}),
+    tests_worked.
 
 area({rectangle, Width, Height}) -> Width * Height;
 area({square, Side}) -> Side * Side.
