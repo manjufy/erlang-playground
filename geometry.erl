46e7cfaea851e6aa6bf767388e1de81ca8843944
Manjunath Reddy <manju16832003@gmail.com>
2020-09-07 21:18:29 +0800

    Extend geometry

--- geometry.erl
+++ geometry.erl
@@ -4,7 +4,9 @@
 test() ->
     12 = area({rectangle, 3, 4}),
     144 = area({square, 12}),
+    3.14159 = area({circle, 1}),
     tests_worked.
 
 area({rectangle, Width, Height}) -> Width * Height;
-area({square, Side}) -> Side * Side.
+area({square, Side})             -> Side * Side;
+area({circle, Radius})           -> 3.14159 * Radius * Radius.
