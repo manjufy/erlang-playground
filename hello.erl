a5a7411c572abb4fa619def772dd528922711501
Manjunath Reddy <manju16832003@gmail.com>
2020-09-13 22:45:50 +0800

    IO format

--- hello.erl
+++ hello.erl
@@ -2,4 +2,6 @@
 -export([print/0]).
 
 print() ->
-    io:format("this outputs one Erlang").
+    io:format("this outputs one Erlang ~n", []),
+    io:format("this outputs one Erlang term: ~w~n", [hello]),
+    io:format("this outputs two Erlang terms: ~w~w~n", [hello, world]).
