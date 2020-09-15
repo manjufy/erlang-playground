284e124c5e49aff75599d81b39426f213c6aeee4
Manjunath Reddy <manju16832003@gmail.com>
2020-09-15 21:48:58 +0800

    Erlang standard output with formatting

---
+++ hello_io.erl
@@ -0,0 +1,18 @@
+-module(hello_io).
+-export([format_temps/1]).
+
+%% only this function is exported
+format_temps([]) ->
+    ok;
+format_temps([City | Rest]) ->
+    print_temp(convert_to_celsius(City)),
+    format_temps(Rest).
+
+convert_to_celsius({Name, {c, Temp}}) ->
+    {Name, {c, Temp}};
+
+convert_to_celsius({Name, {f, Temp}}) ->
+    {Name, {c, (Temp -32) * 5 / 9}}.
+
+print_temp({Name, {c, Temp}}) ->
+    io:format("~-15w ~w c-n", [Name, Temp]).
