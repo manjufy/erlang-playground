c39f17a7fc4c954ab9048cd6c5690f8e6a183d6d
Manjunath Reddy <manju16832003@gmail.com>
2020-09-16 14:07:07 +0800

    IO formatting

--- hello_io.erl
+++ hello_io.erl
@@ -15,4 +15,4 @@ convert_to_celsius({Name, {f, Temp}}) ->
     {Name, {c, (Temp -32) * 5 / 9}}.
 
 print_temp({Name, {c, Temp}}) ->
-    io:format("~-15w ~w c-n", [Name, Temp]).
+    io:format("~-15w ~w c~n", [Name, Temp]).
