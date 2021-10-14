18581292639e2bb0acf0be3f21eef6787a2e56b9
Manjunath Reddy <manju16832003@gmail.com>
2021-10-14 21:00:08 +0800

    update fileserver

--- file-server/afile_server.erl
+++ file-server/afile_server.erl
@@ -1,7 +1,13 @@
+%% To run the program
+%% erl
+%% > c(afile_server).
+%% > FileServer = afile_server.start(".")
+%% > FileServer ! {self(), list_dir}
+%% > receive X -> X end.
 -module(afile_server).
 -export([start/1, loop/1]).
 
-start(Dir) -> spawn(afile_server, loop, [Dir])
+start(Dir) -> spawn(afile_server, loop, [Dir]).
 
 %% In the loop we wait to receive a command; when we receive a command, we obey the command and then call ourselves again to get the next command.
 %% Erlang applies a so-called tail-call optimisationto the code, which means that this function will run in constant space
